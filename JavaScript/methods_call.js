4b37302ab849ddc6c13761950716f5b4ce5cf3c1
OldCapital <55836580+OldCapital@users.noreply.github.com>
2020-05-07 14:41:12 +0300

    lots of crazy mods

---
+++ JavaScript/methods_call.js
@@ -0,0 +1,18 @@
+//=====Lightbox2 method call=====
+
+lightbox.option({
+			  'fadeDuration': 900,
+			  'wrapAround': true,
+			  'alwaysShowNavOnTouchDevices': true,
+			})
+
+//=====Hideseek method call=====
+
+$('#search').hideseek({
+				'list':           '.search',
+		      	'nodata':         'no results',
+		      	'attribute':      'data-title',
+		      	'highlight':      true,
+		      	'navigation':     true,
+		      	'ignore_accents': true,      
+			});
